6c62ab8dec19c718baf49edc21efca8e64b87871
Alberto Dorado Lozano <56201858+aldolo88@users.noreply.github.com>
2023-05-21 19:47:24 +0200

    Add files via upload

---
+++ src/test/java/com/aldolo/sudoku/SudokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldolo.sudoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
